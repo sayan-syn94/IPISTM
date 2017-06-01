ae2627a6f2a5253cb7ed5ba8e3d5cc997458384b
Sayan Basu <devillikeme@gmail.com>
2017-06-01 17:05:23 +0530

    Implemented apriori to mine rules.
    Apriori algorithm is implemented to mine rules from the compressed dataset. Rules are 95% similar to normal apriori with a few extra rules.

--- test5.py
+++ test5.py
@@ -6,10 +6,18 @@ and the later one is deleted from the IFM'''
 import csv
 import bisect
 from operator import add
+from collections import defaultdict
+from itertools import chain, combinations
+import time
+s=time.time()
 transaction=[] #list of all the transactions
+print transaction
 part=[] #'holds all the partitions according to size
 item=[] #'All the items that are available'
+items=set()
+rules=[]
 mat=[] #'The IFM that is generated'
+freqSet=defaultdict(int) #A dictionary that holds the support count of frequent patterns
 'Takes transacions from the csv and converts them into lists'
 def read_t2():
 	transaction.append([3,frozenset([2,3,5]),1])
@@ -31,22 +39,31 @@ def read_t2():
 	item.append(6)
 	item.append(7)
 	item.append(8)
+	items.add(frozenset([1]))
+	items.add(frozenset([2]))
+	items.add(frozenset([3]))
+	items.add(frozenset([4]))
+	items.add(frozenset([5]))
+	items.add(frozenset([6]))
+	items.add(frozenset([7]))
+	items.add(frozenset([8]))
+	print transaction
 
 def transactions():
     count=[]
     tid=1
-    with open('dataset.csv', 'rb') as csvfile:
+    with open('T10I4D100K.csv', 'rb') as csvfile:
     	 spamreader = csv.reader(csvfile)
     	 for row in spamreader:
     		for i in row:
-    			i=int(i)
-    			if i not in item:
-    				item.append(i)
-    			count.append(i)
+				i=int(i)
+				items.add(frozenset([i]))
+				if i not in item:
+					item.append(i)
+				count.append(i)
     		transaction.append([len(count),frozenset(count),tid])
     		count =[];tid=tid+1
     transaction.sort()
-    print 1
 'Partitions the transactions according to size'
 def partition():
 	size=[]
@@ -73,7 +90,7 @@ def gen_matrix():
 	item.sort()
 	item.insert(0,'TID')
 	#print item
-	mat.append([item])
+	mat.append(item)
 	extra=[0]*len(item)
 	for j in range(len(transaction)):
 		extra[0]=+transaction[j][2]
@@ -92,7 +109,7 @@ def ipitm():
 			for x in range(j+1,len(part[i])):
 				if part[i][j][1]==part[i][x][1]:
 					y = part[i][j][0]
-					print y,part[i][x][0]
+					#print y,part[i][x][0]
 					mat[y][1:]=[z+1 if z!=0 else 0 for z in mat[y][1:]]
 					mat[part[i][x][0]][1:]=[z-1 if z!=0 else 0 for z in mat[part[i][x][0]][1:]]
 
@@ -102,13 +119,63 @@ def ipistm():
 		for j in range(1,len(part[i])):
 			for x in range(1,len(part[i+1])):
 				if part[i][j][1].issubset(part[i+1][x][1]):
-					print part[i+1][x][1],part[i][j][1]
-					print mat[part[i+1][x][0]],mat[part[i][j][0]]
+					#print part[i+1][x][1],part[i][j][1]
+					#print mat[part[i+1][x][0]],mat[part[i][j][0]]
 					mat[part[i+1][x][0]][1:]=map(add,mat[part[i+1][x][0]][1:],mat[part[i][j][0]][1:])
 					mat[part[i][j][0]][1:]=[z-z if z!=0 else 0 for z in mat[part[i][j][0]][1:]]
-					print mat[part[i+1][x][0]],mat[part[i][j][0]]
+					#print mat[part[i+1][x][0]],mat[part[i][j][0]]
 					break
+	new_mat = [i for i in mat if set(i[1:]) != set([0])]
+	print len(new_mat)
+	return new_mat
 
+def min_support(itemlist,transaction2,m_supp):
+	s = defaultdict(int)
+	itemSet = set()
+	t = transaction2[0]
+	for i in itemlist:
+		j = [a+1 for a,b in enumerate(t[1:]) if b in i]
+		for k in transaction2[1:]:
+			s[i] += min([k[x] for x in j])
+	for i,v in s.items():
+		support=float(v)/(len(transaction2)-1)
+		if support >= m_supp:
+			itemSet.add(i)
+			freqSet[i] = support
+	return itemSet
+
+
+def subsets(items):
+    """ Returns non empty subsets of items"""
+    return chain(*[combinations(items, i + 1) for i, a in enumerate(items)])
+
+def joinSet(itemSet, length):
+        """Joins sets to return n-length itemset where n=length"""
+        return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])
+
+def run_apriori(minSupport,minConfidence,itemSet,transactionList):
+	l=len(transactionList)
+	largeSet = dict() # Global dictionary which stores (key=n-itemSets,value=support) which satisfy minSupport
+	oneCSet = min_support(itemSet,transactionList,minSupport)
+	currentLSet = oneCSet
+	k = 2
+	i=1
+	while(currentLSet != set([])):
+		largeSet[k-1] = currentLSet
+		currentLSet = joinSet(currentLSet, k)
+		currentCSet = min_support(currentLSet,transactionList,minSupport)
+		currentLSet = currentCSet
+		k = k + 1
+	for key, value in largeSet.items()[1:]:
+		for item in value:
+			_subsets = map(frozenset, [x for x in subsets(item)])
+			for element in _subsets:
+				remain = item.difference(element)
+				if len(remain)> 0:
+					confidence = freqSet[item]/freqSet[element]
+					if confidence >= minConfidence:
+						rules.append([i,element,remain,confidence])
+						i+=1
 
 
 'A binary search function usng the bisect module to find the index number of the item in the item list'
@@ -119,9 +186,23 @@ def bin_sort(n):
     else:
         return -2
 
-transactions()
-#read_t2()
-partition()
-gen_matrix()
-ipitm()
-ipistm()
+def write_csv(data):
+    with open('dataset1-using-IPITSM.csv','wb') as csvfile:
+        writer = csv.writer(csvfile,delimiter=',')
+        for row in data:
+            writer.writerow(row)
+
+
+if __name__ == "__main__":
+
+	minSupport =0.01
+	minConfidence = 0.01
+	#transactions()
+	read_t2()
+	partition()
+	gen_matrix()
+	ipitm()
+	n=ipistm()
+	run_apriori(minSupport, minConfidence,items,n)
+	write_csv(rules)
+	print (s-time.time())
